a6361c9bcf5efecc971960e0a7e6c9ee6f0d31ce
Alistair Singh <alistair.singh7@gmail.com>
2013-08-22 21:46:42 +0200

    Removing all old files

--- Dyno/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DynoWeb.MvcApplication" Language="C#" %>
--- Dyno/Properties/AssemblyInfo.cs
+++ Dyno/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DynoWeb")]
+[assembly: AssemblyTitle("Dyno")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Private")]
-[assembly: AssemblyProduct("DynoWeb")]
-[assembly: AssemblyCopyright("Copyright © Private 2013")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Dyno")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a0b3e2e6-8df7-47e3-85de-e1c1c96f452c")]
+[assembly: Guid("d3e10da9-fa26-40f8-ae4f-c861dbc73209")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
